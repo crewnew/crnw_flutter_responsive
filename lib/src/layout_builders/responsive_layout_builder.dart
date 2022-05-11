dff9473dfdc81cbdfc3e62098311920f9552e8b0
Eusebiu Plesa <eusebiu.plesa@gmail.com>
2022-05-11 11:11:30 +0300

    chore: Initial commit
    push

---
+++ lib/src/layout_builders/responsive_layout_builder.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/widgets.dart';
+
+import '../../crnw_responsive.dart';
+
+/// A widget with a builder that provides you with the screenSizeConfiguration
+///
+/// This widget is used by the ScreenTypeLayoutBuilder to provide different widget builders
+class ResponsiveLayoutBuilder extends StatelessWidget {
+  final Widget Function(BuildContext context, ScreenSizeConfiguration screenSizeConfiguration) builder;
+  final ScreenBreakpoint? screenBreakpoints;
+  final ScreenSizeBreakpoint? screenSizeBreakpoints;
+
+  const ResponsiveLayoutBuilder({
+    required this.builder,
+    this.screenBreakpoints,
+    this.screenSizeBreakpoints,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (context, boxConstraints) => builder(
+          context,
+          ScreenSizeConfiguration(
+            screenType: getScreenType(MediaQuery.of(context).size, screenBreakpoints),
+            sizeType: getSizeType(
+              MediaQuery.of(context).size,
+              screenSizeBreakpoint: screenSizeBreakpoints,
+            ),
+            screenSize: MediaQuery.of(context).size,
+            localWidgetSize: Size(boxConstraints.maxWidth, boxConstraints.maxHeight),
+          ),
+        ),
+      );
+}
